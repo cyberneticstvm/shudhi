9e7905f9ba6eb0c204232dbd150c04f9b58a2421
Cybernetics <mail@cybernetics.me>
2024-02-28 12:30:40 +0530

    small updates

--- routes/web.php
+++ routes/web.php
@@ -20,8 +20,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware(['web'])->group(function () {
     Route::controller(AdminController::class)->group(function () {
-        Route::get('/', 'index')->name('index');
-        Route::get('/login', 'login')->name('login');
+        Route::get('/', 'login')->name('login');
+        Route::get('/shudhiwmsorg', 'index')->name('index');
         Route::post('/login', 'authenticate')->name('authenticate');
         Route::get('/register', 'register')->name('register');
         Route::post('/register', 'signup')->name('signup');
