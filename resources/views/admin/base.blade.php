28cb15b1cdf5b008c1e9f4561a642b8d196373f4
Cybernetics <mail@cybernetics.me>
2024-06-08 18:27:14 +0530

    small updates

--- resources/views/admin/base.blade.php
+++ resources/views/admin/base.blade.php
@@ -153,11 +153,23 @@
     <script src="{{ asset('/backend/assets/js/datatable/buttons/vfs_fonts.js') }}"></script>
     <script src="{{ asset('/backend/assets/js/datatable/buttons/buttons.html5.min.js') }}"></script>
     <script src="{{ asset('/backend/assets/js/datatable/buttons/buttons.print.min.js') }}"></script>-->
+    <script src="https://cdn.datatables.net/v/dt/jq-3.7.0/jszip-3.10.1/dt-2.0.8/b-3.0.2/b-html5-3.0.2/datatables.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/pdfmake.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/vfs_fonts.js"></script>
-    <script src="https://cdn.datatables.net/v/dt/jq-3.7.0/jszip-3.10.1/dt-2.0.8/b-3.0.2/b-html5-3.0.2/datatables.min.js"></script>
-    <script src="{{ asset('/backend/assets/js/datatable/datatables/datatable.custom.js') }}"></script>
 
+    <!--<script src="{{ asset('/backend/assets/js/datatable/datatables/datatable.custom.js') }}"></script>-->
+    <script>
+        $(function() {
+            $('#basic-2').DataTable({
+                buttons: [
+                    'excel'
+                ],
+                "paging": true,
+                "ordering": false,
+                "info": false
+            });
+        })
+    </script>
     @include("message")
 </body>
 
