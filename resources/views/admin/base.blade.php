94a71a62e193699c1ff555ba3c73027f21a18ebc
Cybernetics <mail@cybernetics.me>
2024-06-08 17:53:14 +0530

    small updates

--- resources/views/admin/base.blade.php
+++ resources/views/admin/base.blade.php
@@ -139,6 +139,16 @@
     <script src="{{ asset('/backend/assets/js/select2/select2-custom.js') }}"></script>
     <script src="{{ asset('/backend/assets/js/todo/todo.js') }}"></script>
     <!-- Plugins JS Ends-->
+
+    <script src="https://cdn.datatables.net/buttons/3.0.2/js/dataTables.buttons.js"></script>
+    <script src="https://cdn.datatables.net/buttons/3.0.2/js/buttons.bootstrap5.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/pdfmake.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/vfs_fonts.js"></script>
+    <script src="https://cdn.datatables.net/buttons/3.0.2/js/buttons.html5.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/3.0.2/js/buttons.print.min.js"></script>
+
+
     <!-- Theme js-->
     <script src="{{ asset('/backend/assets/js/script.js') }}"></script>
     <script src="{{ asset('/backend/assets/js/mpp.js') }}"></script>
